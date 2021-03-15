1cc14d780e6356a906aec4e312d52ec741182c45
billy rennekamp <billy.rennekamp@gmail.com>
2021-03-15 18:43:38 +0100

    hardhat still doesnt work

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,7 +12,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.5.0',
+        version: '0.5.1',
         settings: {
           optimizer: {
             enabled: false,
