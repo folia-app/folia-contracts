03425bd10d38289d5a35ceda4e27ef93a2a77f64
billy rennekamp <billy.rennekamp@gmail.com>
2021-03-16 19:27:08 +0100

    pause

--- contracts/FoliaController.sol
+++ contracts/FoliaController.sol
@@ -1,124 +1,124 @@
-pragma solidity ^0.5.0;
+// pragma solidity ^0.5.0;
 
-/**
- * The FoliaController is an upgradeable endpoint for controlling Folia.sol
- */
+// /**
+//  * The FoliaController is an upgradeable endpoint for controlling Folia.sol
+//  */
 
-import "./Folia.sol";
-import "openzeppelin-solidity/contracts/math/SafeMath.sol";
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+// import "./Folia.sol";
+// import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+// import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract FoliaController is Ownable {
+// contract FoliaController is Ownable {
 
-    event newWork(uint256 workId, address payable artist, uint256 editions, uint256 price, bool paused);
-    event updatedWork(uint256 workId, address payable artist, uint256 editions, uint256 price, bool paused);
-    event editionBought(uint256 workId, uint256 editionId, uint256 tokenId, address recipient, uint256 paid, uint256 artistReceived, uint256 adminReceived);
+//     event newWork(uint256 workId, address payable artist, uint256 editions, uint256 price, bool paused);
+//     event updatedWork(uint256 workId, address payable artist, uint256 editions, uint256 price, bool paused);
+//     event editionBought(uint256 workId, uint256 editionId, uint256 tokenId, address recipient, uint256 paid, uint256 artistReceived, uint256 adminReceived);
 
-    using SafeMath for uint256;
+//     using SafeMath for uint256;
 
-    uint256 constant MAX_EDITIONS = 1000000;
-    uint256 public latestWorkId;
+//     uint256 constant MAX_EDITIONS = 1000000;
+//     uint256 public latestWorkId;
 
-    mapping (uint256 => Work) public works;
-    struct Work {
-        bool exists;
-        bool paused;
-        uint256 editions;
-        uint256 printed;
-        uint256 price;
-        address payable artist;
-    }
+//     mapping (uint256 => Work) public works;
+//     struct Work {
+//         bool exists;
+//         bool paused;
+//         uint256 editions;
+//         uint256 printed;
+//         uint256 price;
+//         address payable artist;
+//     }
 
-    uint256 public adminSplit = 15;
+//     uint256 public adminSplit = 15;
 
-    address payable public adminWallet;
-    bool public paused;
-    Folia public folia;
+//     address payable public adminWallet;
+//     bool public paused;
+//     Folia public folia;
 
-    modifier notPaused() {
-        require(!paused, "Must not be paused");
-        _;
-    }
+//     modifier notPaused() {
+//         require(!paused, "Must not be paused");
+//         _;
+//     }
 
-    constructor(
-        Folia _folia,
-        address payable _adminWallet
-    ) public {
-        folia = _folia;
-        adminWallet = _adminWallet;
-    }
+//     constructor(
+//         Folia _folia,
+//         address payable _adminWallet
+//     ) public {
+//         folia = _folia;
+//         adminWallet = _adminWallet;
+//     }
 
-    function addArtwork(address payable artist, uint256 editions, uint256 price, bool _paused) public onlyOwner {
-        require(editions < MAX_EDITIONS, "MAX_EDITIONS_EXCEEDED");
+//     function addArtwork(address payable artist, uint256 editions, uint256 price, bool _paused) public onlyOwner {
+//         require(editions < MAX_EDITIONS, "MAX_EDITIONS_EXCEEDED");
 
-        latestWorkId += 1;
+//         latestWorkId += 1;
 
-        works[latestWorkId].exists = true;
-        works[latestWorkId].editions = editions;
-        works[latestWorkId].price = price;
-        works[latestWorkId].artist = artist;
-        works[latestWorkId].paused = _paused;
-        emit newWork(latestWorkId, artist, editions, price, _paused);
-    }
+//         works[latestWorkId].exists = true;
+//         works[latestWorkId].editions = editions;
+//         works[latestWorkId].price = price;
+//         works[latestWorkId].artist = artist;
+//         works[latestWorkId].paused = _paused;
+//         emit newWork(latestWorkId, artist, editions, price, _paused);
+//     }
 
-    function updateArtworkPaused(uint256 workId, bool _paused) public onlyOwner {
-        require(works[workId].exists, "WORK_DOES_NOT_EXIST");
-        works[workId].paused = _paused;
-        emit updatedWork(workId, works[workId].artist, works[workId].editions, works[workId].price, works[workId].paused);
-    }
+//     function updateArtworkPaused(uint256 workId, bool _paused) public onlyOwner {
+//         require(works[workId].exists, "WORK_DOES_NOT_EXIST");
+//         works[workId].paused = _paused;
+//         emit updatedWork(workId, works[workId].artist, works[workId].editions, works[workId].price, works[workId].paused);
+//     }
 
-    function updateArtworkEditions(uint256 workId, uint256 _editions) public onlyOwner {
-        require(works[workId].exists, "WORK_DOES_NOT_EXIST");
-        require(works[workId].printed < _editions, "WORK_EXCEEDS_EDITIONS");
-        works[workId].editions = _editions;
-        emit updatedWork(workId, works[workId].artist, works[workId].editions, works[workId].price, works[workId].paused);
-    }
+//     function updateArtworkEditions(uint256 workId, uint256 _editions) public onlyOwner {
+//         require(works[workId].exists, "WORK_DOES_NOT_EXIST");
+//         require(works[workId].printed < _editions, "WORK_EXCEEDS_EDITIONS");
+//         works[workId].editions = _editions;
+//         emit updatedWork(workId, works[workId].artist, works[workId].editions, works[workId].price, works[workId].paused);
+//     }
 
-    function updateArtworkPrice(uint256 workId, uint256 _price) public onlyOwner {
-        require(works[workId].exists, "WORK_DOES_NOT_EXIST");
-        works[workId].price = _price;
-        emit updatedWork(workId, works[workId].artist, works[workId].editions, works[workId].price, works[workId].paused);
-    }
+//     function updateArtworkPrice(uint256 workId, uint256 _price) public onlyOwner {
+//         require(works[workId].exists, "WORK_DOES_NOT_EXIST");
+//         works[workId].price = _price;
+//         emit updatedWork(workId, works[workId].artist, works[workId].editions, works[workId].price, works[workId].paused);
+//     }
 
-    function updateArtworkArtist(uint256 workId, address payable _artist) public onlyOwner {
-        require(works[workId].exists, "WORK_DOES_NOT_EXIST");
-        works[workId].artist = _artist;
-        emit updatedWork(workId, works[workId].artist, works[workId].editions, works[workId].price, works[workId].paused);
-    }
+//     function updateArtworkArtist(uint256 workId, address payable _artist) public onlyOwner {
+//         require(works[workId].exists, "WORK_DOES_NOT_EXIST");
+//         works[workId].artist = _artist;
+//         emit updatedWork(workId, works[workId].artist, works[workId].editions, works[workId].price, works[workId].paused);
+//     }
 
-    function buy(address recipient, uint256 workId) public payable notPaused returns (bool) {
-        require(!works[workId].paused, "WORK_NOT_YET_FOR_SALE");
-        require(works[workId].exists, "WORK_DOES_NOT_EXIST");
-        require(works[workId].editions > works[workId].printed, "EDITIONS_EXCEEDED");
-        require(msg.value == works[workId].price, "DID_NOT_SEND_PRICE");
+//     function buy(address recipient, uint256 workId) public payable notPaused returns (bool) {
+//         require(!works[workId].paused, "WORK_NOT_YET_FOR_SALE");
+//         require(works[workId].exists, "WORK_DOES_NOT_EXIST");
+//         require(works[workId].editions > works[workId].printed, "EDITIONS_EXCEEDED");
+//         require(msg.value == works[workId].price, "DID_NOT_SEND_PRICE");
 
-        uint256 editionId = works[workId].printed.add(1);
-        works[workId].printed = editionId;
+//         uint256 editionId = works[workId].printed.add(1);
+//         works[workId].printed = editionId;
         
-        uint256 tokenId = workId.mul(MAX_EDITIONS).add(editionId);
+//         uint256 tokenId = workId.mul(MAX_EDITIONS).add(editionId);
 
-        folia.mint(recipient, tokenId);
+//         folia.mint(recipient, tokenId);
         
-        uint256 adminReceives = msg.value.mul(adminSplit).div(100);
-        uint256 artistReceives = msg.value.sub(adminReceives);
+//         uint256 adminReceives = msg.value.mul(adminSplit).div(100);
+//         uint256 artistReceives = msg.value.sub(adminReceives);
 
-        adminWallet.transfer(adminReceives);
-        works[workId].artist.transfer(artistReceives);
+//         adminWallet.transfer(adminReceives);
+//         works[workId].artist.transfer(artistReceives);
 
-        emit editionBought(workId, editionId, tokenId, recipient,  works[workId].price, artistReceives, adminReceives);
-    }
+//         emit editionBought(workId, editionId, tokenId, recipient,  works[workId].price, artistReceives, adminReceives);
+//     }
 
-    function updateAdminSplit(uint256 _adminSplit) public onlyOwner {
-        require(_adminSplit <= 100, "SPLIT_MUST_BE_LTE_100");
-        adminSplit = _adminSplit;
-    }
+//     function updateAdminSplit(uint256 _adminSplit) public onlyOwner {
+//         require(_adminSplit <= 100, "SPLIT_MUST_BE_LTE_100");
+//         adminSplit = _adminSplit;
+//     }
 
-    function updateAdminWallet(address payable _adminWallet) public onlyOwner {
-        adminWallet = _adminWallet;
-    }
+//     function updateAdminWallet(address payable _adminWallet) public onlyOwner {
+//         adminWallet = _adminWallet;
+//     }
 
-    function updatePaused(bool _paused) public onlyOwner {
-        paused = _paused;
-    }
+//     function updatePaused(bool _paused) public onlyOwner {
+//         paused = _paused;
+//     }
 
-}
+// }
--- contracts/ReserveAuction.sol
+++ contracts/ReserveAuction.sol
@@ -1,237 +1,237 @@
-pragma solidity ^0.5.0;
-pragma experimental ABIEncoderV2;
+// pragma solidity ^0.5.0;
+// pragma experimental ABIEncoderV2;
 
-import "openzeppelin-solidity/contracts/math/SafeMath.sol";
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+// import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+// import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-import "openzeppelin-solidity/contracts/token/ERC721/IERC721.sol";
-import "openzeppelin-solidity/contracts/introspection/IERC165.sol";
-import "openzeppelin-solidity/contracts/utils/ReentrancyGuard.sol";
+// import "openzeppelin-solidity/contracts/token/ERC721/IERC721.sol";
+// import "openzeppelin-solidity/contracts/introspection/IERC165.sol";
+// import "openzeppelin-solidity/contracts/utils/ReentrancyGuard.sol";
 
-/**
-   _____                                                   _   _                           
-  |  __ \                                  /\             | | (_)                          
-  | |__) |___ ___  ___ _ ____   _____     /  \  _   _  ___| |_ _  ___  _ __                
-  |  _  // _ / __|/ _ | '__\ \ / / _ \   / /\ \| | | |/ __| __| |/ _ \| '_ \               
-  | | \ |  __\__ |  __| |   \ V |  __/  / ____ | |_| | (__| |_| | (_) | | | |              
-  |_|  \_\___|___/\___|_|    \_/ \___| /_/    \_\__,_|\___|\__|_|\___/|_| |_|              
+// /**
+//    _____                                                   _   _                           
+//   |  __ \                                  /\             | | (_)                          
+//   | |__) |___ ___  ___ _ ____   _____     /  \  _   _  ___| |_ _  ___  _ __                
+//   |  _  // _ / __|/ _ | '__\ \ / / _ \   / /\ \| | | |/ __| __| |/ _ \| '_ \               
+//   | | \ |  __\__ |  __| |   \ V |  __/  / ____ | |_| | (__| |_| | (_) | | | |              
+//   |_|  \_\___|___/\___|_|    \_/ \___| /_/    \_\__,_|\___|\__|_|\___/|_| |_|              
                                                                                            
                                                                                            
-   ____          ____  _ _ _         _____                       _                         
-  |  _ \        |  _ \(_| | |       |  __ \                     | |                        
-  | |_) |_   _  | |_) |_| | |_   _  | |__) |___ _ __  _ __   ___| | ____ _ _ __ ___  _ __  
-  |  _ <| | | | |  _ <| | | | | | | |  _  // _ | '_ \| '_ \ / _ | |/ / _` | '_ ` _ \| '_ \ 
-  | |_) | |_| | | |_) | | | | |_| | | | \ |  __| | | | | | |  __|   | (_| | | | | | | |_) |
-  |____/ \__, | |____/|_|_|_|\__, | |_|  \_\___|_| |_|_| |_|\___|_|\_\__,_|_| |_| |_| .__/ 
-          __/ |               __/ |                                                 | |    
-         |___/               |___/                                                  |_|    
+//    ____          ____  _ _ _         _____                       _                         
+//   |  _ \        |  _ \(_| | |       |  __ \                     | |                        
+//   | |_) |_   _  | |_) |_| | |_   _  | |__) |___ _ __  _ __   ___| | ____ _ _ __ ___  _ __  
+//   |  _ <| | | | |  _ <| | | | | | | |  _  // _ | '_ \| '_ \ / _ | |/ / _` | '_ ` _ \| '_ \ 
+//   | |_) | |_| | | |_) | | | | |_| | | | \ |  __| | | | | | |  __|   | (_| | | | | | | |_) |
+//   |____/ \__, | |____/|_|_|_|\__, | |_|  \_\___|_| |_|_| |_|\___|_|\_\__,_|_| |_| |_| .__/ 
+//           __/ |               __/ |                                                 | |    
+//          |___/               |___/                                                  |_|    
 
-*/
+// */
 
-contract ReserveAuction is Ownable, ReentrancyGuard {
-    using SafeMath for uint256;
+// contract ReserveAuction is Ownable, ReentrancyGuard {
+//     using SafeMath for uint256;
 
-    bool public paused;
+//     bool public paused;
 
-    uint256 public timeBuffer = 15 * 60; // extend 15 minutes after every bid made in last 15 minutes
-    uint256 public minBid = 1 * 10**17; // 0.1 eth
+//     uint256 public timeBuffer = 15 * 60; // extend 15 minutes after every bid made in last 15 minutes
+//     uint256 public minBid = 1 * 10**17; // 0.1 eth
 
-    bytes4 constant interfaceId = 0x80ac58cd; // 721 interface id
-    address public nftAddress;
+//     bytes4 constant interfaceId = 0x80ac58cd; // 721 interface id
+//     address public nftAddress;
 
-    mapping(uint256 => Auction) public auctions;
-    uint256[] public tokenIds;
+//     mapping(uint256 => Auction) public auctions;
+//     uint256[] public tokenIds;
 
-    struct Auction {
-        bool exists;
-        uint256 amount;
-        uint256 tokenId;
-        uint256 duration;
-        uint256 firstBidTime;
-        uint256 reservePrice;
-        address payable creator;
-        address payable bidder;
-    }
+//     struct Auction {
+//         bool exists;
+//         uint256 amount;
+//         uint256 tokenId;
+//         uint256 duration;
+//         uint256 firstBidTime;
+//         uint256 reservePrice;
+//         address payable creator;
+//         address payable bidder;
+//     }
 
-    modifier notPaused() {
-        require(!paused, "Must not be paused");
-        _;
-    }
+//     modifier notPaused() {
+//         require(!paused, "Must not be paused");
+//         _;
+//     }
 
-    event AuctionCreated(
-        uint256 tokenId,
-        address nftAddress,
-        uint256 duration,
-        uint256 reservePrice,
-        address creator
-    );
-    event AuctionBid(
-        uint256 tokenId,
-        address nftAddress,
-        address sender,
-        uint256 value,
-        uint256 timestamp,
-        bool firstBid,
-        bool extended
-    );
-    event AuctionEnded(
-        uint256 tokenId,
-        address nftAddress,
-        address creator,
-        address winner,
-        uint256 amount
-    );
-    event AuctionCanceled(
-        uint256 tokenId,
-        address nftAddress,
-        address creator
-    );
+//     event AuctionCreated(
+//         uint256 tokenId,
+//         address nftAddress,
+//         uint256 duration,
+//         uint256 reservePrice,
+//         address creator
+//     );
+//     event AuctionBid(
+//         uint256 tokenId,
+//         address nftAddress,
+//         address sender,
+//         uint256 value,
+//         uint256 timestamp,
+//         bool firstBid,
+//         bool extended
+//     );
+//     event AuctionEnded(
+//         uint256 tokenId,
+//         address nftAddress,
+//         address creator,
+//         address winner,
+//         uint256 amount
+//     );
+//     event AuctionCanceled(
+//         uint256 tokenId,
+//         address nftAddress,
+//         address creator
+//     );
 
-    constructor(address _nftAddress) public {
-        require(
-            IERC165(_nftAddress).supportsInterface(interfaceId),
-            "Doesn't support NFT interface"
-        );
-        nftAddress = _nftAddress;
-    }
+//     constructor(address _nftAddress) public {
+//         require(
+//             IERC165(_nftAddress).supportsInterface(interfaceId),
+//             "Doesn't support NFT interface"
+//         );
+//         nftAddress = _nftAddress;
+//     }
 
-    function updateNftAddress(address _nftAddress) public onlyOwner {
-        require(
-            IERC165(_nftAddress).supportsInterface(interfaceId),
-            "Doesn't support NFT interface"
-        );
-        nftAddress = _nftAddress;
-    }
+//     function updateNftAddress(address _nftAddress) public onlyOwner {
+//         require(
+//             IERC165(_nftAddress).supportsInterface(interfaceId),
+//             "Doesn't support NFT interface"
+//         );
+//         nftAddress = _nftAddress;
+//     }
 
-    function updateMinBid(uint256 _minBid) public onlyOwner {
-        minBid = _minBid;
-    }
+//     function updateMinBid(uint256 _minBid) public onlyOwner {
+//         minBid = _minBid;
+//     }
 
-    function updateTimeBuffer(uint256 _timeBuffer) public onlyOwner {
-        timeBuffer = _timeBuffer;
-    }
+//     function updateTimeBuffer(uint256 _timeBuffer) public onlyOwner {
+//         timeBuffer = _timeBuffer;
+//     }
 
-    function createAuction(
-        uint256 tokenId,
-        uint256 duration,
-        uint256 reservePrice,
-        address payable creator
-    ) external notPaused onlyOwner {
-        require(!auctions[tokenId].exists, "Auction already exists");
+//     function createAuction(
+//         uint256 tokenId,
+//         uint256 duration,
+//         uint256 reservePrice,
+//         address payable creator
+//     ) external notPaused onlyOwner {
+//         require(!auctions[tokenId].exists, "Auction already exists");
 
-        tokenIds.push(tokenId);
+//         tokenIds.push(tokenId);
 
-        auctions[tokenId].exists = true;
-        auctions[tokenId].duration = duration;
-        auctions[tokenId].reservePrice = reservePrice;
-        auctions[tokenId].creator = creator;
+//         auctions[tokenId].exists = true;
+//         auctions[tokenId].duration = duration;
+//         auctions[tokenId].reservePrice = reservePrice;
+//         auctions[tokenId].creator = creator;
 
-        IERC721(nftAddress).transferFrom(creator, address(this), tokenId);
+//         IERC721(nftAddress).transferFrom(creator, address(this), tokenId);
 
-        emit AuctionCreated(tokenId, nftAddress, duration, reservePrice, creator);
-    }
+//         emit AuctionCreated(tokenId, nftAddress, duration, reservePrice, creator);
+//     }
 
-    function createBid(uint256 tokenId) external payable notPaused {
-        require(auctions[tokenId].exists, "Auction doesn't exist");
-        require(
-            msg.value >= auctions[tokenId].reservePrice,
-            "Must send reservePrice or more"
-        );
-        require(
-            auctions[tokenId].firstBidTime == 0 ||
-                block.timestamp <
-                auctions[tokenId].firstBidTime + auctions[tokenId].duration,
-            "Auction expired"
-        );
+//     function createBid(uint256 tokenId) external payable notPaused {
+//         require(auctions[tokenId].exists, "Auction doesn't exist");
+//         require(
+//             msg.value >= auctions[tokenId].reservePrice,
+//             "Must send reservePrice or more"
+//         );
+//         require(
+//             auctions[tokenId].firstBidTime == 0 ||
+//                 block.timestamp <
+//                 auctions[tokenId].firstBidTime + auctions[tokenId].duration,
+//             "Auction expired"
+//         );
 
-        uint256 lastValue = auctions[tokenId].amount;
+//         uint256 lastValue = auctions[tokenId].amount;
 
-        bool firstBid;
-        address payable lastBidder;
+//         bool firstBid;
+//         address payable lastBidder;
 
-        // allows for auctions with starting price of 0
-        if (lastValue != 0) {
-            require(msg.value > lastValue, "Must send more than last bid");
-            require(
-                msg.value.sub(lastValue) >= minBid,
-                "Must send more than last bid by minBid Amount"
-            );
-            lastBidder = auctions[tokenId].bidder;
-        } else {
-            firstBid = true;
-            auctions[tokenId].firstBidTime = block.timestamp;
-        }
+//         // allows for auctions with starting price of 0
+//         if (lastValue != 0) {
+//             require(msg.value > lastValue, "Must send more than last bid");
+//             require(
+//                 msg.value.sub(lastValue) >= minBid,
+//                 "Must send more than last bid by minBid Amount"
+//             );
+//             lastBidder = auctions[tokenId].bidder;
+//         } else {
+//             firstBid = true;
+//             auctions[tokenId].firstBidTime = block.timestamp;
+//         }
 
-        auctions[tokenId].amount = msg.value;
-        auctions[tokenId].bidder = msg.sender;
+//         auctions[tokenId].amount = msg.value;
+//         auctions[tokenId].bidder = msg.sender;
 
-        bool extended;
-        // at this point we know that the timestamp is less than start + duration
-        // we want to know by how much the timestamp is less than start + duration
-        // if the difference is less than the timeBuffer, increase the duration by the timeBuffer
-        if (
-            (auctions[tokenId].firstBidTime.add(auctions[tokenId].duration))
-                .sub(block.timestamp) < timeBuffer
-        ) {
-            auctions[tokenId].duration += timeBuffer;
-            extended = true;
-        }
+//         bool extended;
+//         // at this point we know that the timestamp is less than start + duration
+//         // we want to know by how much the timestamp is less than start + duration
+//         // if the difference is less than the timeBuffer, increase the duration by the timeBuffer
+//         if (
+//             (auctions[tokenId].firstBidTime.add(auctions[tokenId].duration))
+//                 .sub(block.timestamp) < timeBuffer
+//         ) {
+//             auctions[tokenId].duration += timeBuffer;
+//             extended = true;
+//         }
 
-        if (!firstBid) {
-            lastBidder.transfer(lastValue);
-        }
+//         if (!firstBid) {
+//             lastBidder.transfer(lastValue);
+//         }
 
-        emit AuctionBid(
-            tokenId,
-            nftAddress,
-            msg.sender,
-            msg.value,
-            block.timestamp,
-            firstBid,
-            extended
-        );
-    }
+//         emit AuctionBid(
+//             tokenId,
+//             nftAddress,
+//             msg.sender,
+//             msg.value,
+//             block.timestamp,
+//             firstBid,
+//             extended
+//         );
+//     }
 
-    function endAuction(uint256 tokenId) external notPaused {
-        require(auctions[tokenId].exists, "Auction doesn't exist");
-        require(
-            uint256(auctions[tokenId].firstBidTime) != 0,
-            "Auction hasn't begun"
-        );
-        require(
-            block.timestamp >=
-                auctions[tokenId].firstBidTime + auctions[tokenId].duration,
-            "Auction hasn't completed"
-        );
+//     function endAuction(uint256 tokenId) external notPaused {
+//         require(auctions[tokenId].exists, "Auction doesn't exist");
+//         require(
+//             uint256(auctions[tokenId].firstBidTime) != 0,
+//             "Auction hasn't begun"
+//         );
+//         require(
+//             block.timestamp >=
+//                 auctions[tokenId].firstBidTime + auctions[tokenId].duration,
+//             "Auction hasn't completed"
+//         );
 
-        address winner = auctions[tokenId].bidder;
-        uint256 amount = auctions[tokenId].amount;
-        address payable creator = auctions[tokenId].creator;
+//         address winner = auctions[tokenId].bidder;
+//         uint256 amount = auctions[tokenId].amount;
+//         address payable creator = auctions[tokenId].creator;
 
-        emit AuctionEnded(tokenId, nftAddress, creator, winner, amount);
-        delete auctions[tokenId];
+//         emit AuctionEnded(tokenId, nftAddress, creator, winner, amount);
+//         delete auctions[tokenId];
 
-        IERC721(nftAddress).transferFrom(address(this), winner, tokenId);
-        creator.transfer(amount);
-    }
+//         IERC721(nftAddress).transferFrom(address(this), winner, tokenId);
+//         creator.transfer(amount);
+//     }
 
-    function cancelAuction(uint256 tokenId) external {
-        require(auctions[tokenId].exists, "Auction doesn't exist");
-        require(
-            auctions[tokenId].creator == msg.sender || msg.sender == owner(),
-            "Can only be called by auction creator or owner"
-        );
-        require(
-            uint256(auctions[tokenId].firstBidTime) == 0,
-            "Can't cancel an auction once it's begun"
-        );
-        address creator = auctions[tokenId].creator;
-        IERC721(nftAddress).transferFrom(address(this), creator, tokenId);
-        emit AuctionCanceled(tokenId, nftAddress, creator);
-        delete auctions[tokenId];
-    }
+//     function cancelAuction(uint256 tokenId) external {
+//         require(auctions[tokenId].exists, "Auction doesn't exist");
+//         require(
+//             auctions[tokenId].creator == msg.sender || msg.sender == owner(),
+//             "Can only be called by auction creator or owner"
+//         );
+//         require(
+//             uint256(auctions[tokenId].firstBidTime) == 0,
+//             "Can't cancel an auction once it's begun"
+//         );
+//         address creator = auctions[tokenId].creator;
+//         IERC721(nftAddress).transferFrom(address(this), creator, tokenId);
+//         emit AuctionCanceled(tokenId, nftAddress, creator);
+//         delete auctions[tokenId];
+//     }
 
-    function updatePaused(bool _paused) public onlyOwner {
-        paused = _paused;
-    }
-}
+//     function updatePaused(bool _paused) public onlyOwner {
+//         paused = _paused;
+//     }
+// }
