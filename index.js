b9a942ee2fbab5f417498fa1cb743847cde5f772
billy rennekamp <billy.rennekamp@gmail.com>
2021-04-25 19:23:33 +0200

    export v2

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var Folia = require('./build/contracts/Folia.json')
 var FoliaController = require('./build/contracts/FoliaController.json')
+var FoliaControllerV2 = require('./build/contracts/FoliaControllerV2.json')
 var Metadata = require('./build/contracts/Metadata.json')
 var ReserveAuction = require('./build/contracts/ReserveAuction.json')
 
 module.exports = {
   Folia,
   FoliaController,
+  FoliaControllerV2,
   Metadata,
   ReserveAuction
 }
