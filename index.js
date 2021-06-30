761f45da51cf37ce19d6df77744b68829e8f1b42
billy rennekamp <billy.rennekamp@gmail.com>
2021-06-30 19:33:06 +0200

    helps?

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-export * from './typechain';
-export * from './utils';
+// export * from './typechain';
+// export * from './utils';
 
 var Folia = require('./build/contracts/Folia.json')
 var FoliaController = require('./build/contracts/FoliaController.json')
